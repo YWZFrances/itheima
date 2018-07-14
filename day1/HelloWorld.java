057513bb50a3b518e476ed49c43ba418a50fa229
YWZFrances <1595642024@qq.com>
2018-07-14 22:45:23 +0800

    123

---
+++ day1/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld!!!!!");
+	}
+}
+变量是内存中的一小块区域
+在程序的执行过程中，其值可以在一定范围内发生改变
+A:对区域要有限定(用数据类型来限定)
+B:必须对区域给一个名称：；变量名
+C:区域内必须要有内容：初始化值
+
+定义格式
+	数据类型 变量名 = 初始化值；
---
+++ day1/VariableDemo.java
@@ -0,0 +1,24 @@
+public class VariableDemo{
+	public static void main(String[] args){
+		//byte
+		byte b = 10;
+		System.out.println(10);
+		System.out.println(b);
+		
+		//short
+		short s = 100;
+		System.out.println(s);
+
+		//int
+		int i = 1000;
+		System.out.println(i);
+
+		//long
+		long l = 10000000000000000L;
+		System.out.println(l);
+
+	}
+}
+数据类型 变量名 = 初始化值
+定义long类型变量的时候，最好直接加L
+定义float类型变量的时候，最好直接加F
